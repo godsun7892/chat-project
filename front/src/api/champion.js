acabd8edf73aa46fb5294c3c1df96726446ab415
유광우 <godsun7892@naver.com>
2024-04-08 22:35:22 +0900

    delete : front

--- front/src/api/champion.js
+++
@@ -1,9 +0,0 @@
-import { localAxios } from "@/utils/http-commons";
-
-const local = localAxios;
-
-async function championInfo(){
-    return local.get(`/champions`);
-}
-
-export { championInfo }
--- front/src/api/predictRandomMatch.js
+++
@@ -1,9 +0,0 @@
-import { localAxios } from "@/utils/http-commons";
-
-const local = localAxios;
-
-function randomMatch() {
-  return local.get(`/randomMatch`);
-}
-
-export { randomMatch };
